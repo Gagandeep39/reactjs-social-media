eb9179571c675d168a47660a7108d04809dc787a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-05 18:34:16 +0530

    Added logic to send user data on sccessfull response

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../../middleware/auth');
+const Users = require('../../models/Users');
 
 /**
  * @route GET /api/users
  * @description Test Route
  * @access Public
  */
-router.get('/', (req, res) => res.send('Auth Route'));
+// TO use a middleware we add it as a parameter to express sj
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const user = await Users.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
