1349acd9f9e629e1a85b36bd35ff77409dd21275
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-28 18:04:58 +0530

    IMplemented a basic profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth')
+const User = require('../../models/Users')
+const Profile = require('../../models/Profile')
 
 /**
- * @route GET /api/users
- * @description Test Route
- * @access Public
+ * @route GET /api/profile/me
+ * @description Get Current user profile
+ * @access Private
  */
-router.get('/', (req, res) => res.send('Profile Route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar'])
+    if(!profile){
+      return res.status(400).send({msg: 'Profile not found'})
+    }
+    res.send(profile)
+  } catch (error) {
+    res.status(500).send('Server Error')
+  }
+});
 module.exports = router;
