ccd063971449bc6a22cbf413b9dab859d9495567
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-29 22:33:33 +0530

    Implemented routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,25 @@ import React, { Fragment } from 'react';
 import './App.css';
 import { Navbar } from './components/layouts/Navbar';
 import { Landing } from './components/layouts/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 
 function App() {
   return (
-    <Fragment className='App'>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment className='App'>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          {/* Works like switch case and nly renders th first matching path */}
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 }
 
