379475230d91675178e8e1cf8ed647c5609a8f74
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-29 23:08:28 +0530

    Implemented register connection logic Created login component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -14,10 +15,29 @@ export const Register = () => {
   const onChange = (event) =>
     setFormData({ ...formData, [event.target.name]: event.target.value });
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
     if (password !== password2) console.log('Password do not match');
-    else console.log('Form Data');
+    else {
+      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            'Content-type': 'application/json'
+          }
+        }
+        const body = JSON.stringify(newUser);
+        const res = await axios.post('/api/users', body, config)
+        console.log(res.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    }
   };
 
   return (
