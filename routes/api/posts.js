c0c0f3762e2699a214bb1cfcb275764b374e0458
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-29 14:13:45 +0530

    Implemented create  post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Users = require('../../models/Users');
 
 /**
- * @route GET /api/users
+ * @route GET /api/posts
  * @description Test Route
- * @access Public
+ * @access Create a post
  */
-router.get('/', (req, res) => res.send('Users Route'));
+router.post(
+  '/',
+  [auth, [body('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // -password will prevent password from fetching
+      const user = await Users.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: user.id,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 module.exports = router;
