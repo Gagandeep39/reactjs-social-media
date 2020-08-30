633fdf1291dbe2deae0d36973dbfdc2b269aab80
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-30 18:12:11 +0530

    Implemenyted dummy redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,26 @@ import { Landing } from './components/layouts/Landing';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <Fragment className='App'>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          {/* Works like switch case and nly renders th first matching path */}
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment className='App'>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            {/* Works like switch case and nly renders th first matching path */}
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+// Will look for file named index.js inside reducer folder
+import rootRoducer from './reducers';
+
+const initialState = {}
+const middleware = [thunk]
+const store = createStore(
+  rootRoducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
