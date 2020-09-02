cb99375ddf2d769ec6913fd49d952eb0dd48bf83
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-02 10:43:02 +0530

    Implemwnted profiles component

--- client/src/components/profile/Profiles.js
+++ client/src/components/profile/Profiles.js
@@ -1,14 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAllProfiles } from '../../store/actions/profile';
 import Spinner from '../layouts/Spinner';
+import ProfileItem from './ProfileItem';
 
 const Profiles = ({ getAllProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getAllProfiles();
   }, []);
-  return <div></div>;
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className='large text-primary'> Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop'></i> Browse and connect with
+            developers
+          </p>
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No Profiles Found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
