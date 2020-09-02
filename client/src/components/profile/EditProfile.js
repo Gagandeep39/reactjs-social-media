765d8ea0561038ce5b03d9270b4af51f0c2028aa
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-02 10:44:41 +0530

    Fixed warnings

--- client/src/components/profile/EditProfile.js
+++ client/src/components/profile/EditProfile.js
@@ -62,7 +62,7 @@ const EditProfile = ({
       youtube: loading || !profile.social ? '' : profile.social.youtube,
       instagram: loading || !profile.social ? '' : profile.social.instagram,
     });
-  }, [loading]);
+  }, [loading, getCurrentProfile, profile]);
 
   const onChange = (event) =>
     setFormData({ ...formData, [event.target.name]: event.target.value });
