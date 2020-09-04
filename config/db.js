890617b4e3b958251a11e3fa20491ba7e540e494
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-04 14:03:41 +0530

    Improved DB connection code

--- config/db.js
+++ config/db.js
@@ -1,30 +1,27 @@
-// Stores Database Connection connect
-// Import Mongoose library
 const mongoose = require('mongoose');
-// Import Config package to to access configs eg. default.json file
-const config = require('config');
-// Get mongo db instance from config
-// Look for key value pair `mongoURI`
-// Value is available in MongoDB cloud server
+const keys = require('../config/keys');
 // Cluster -> Select your cluster -> Connect -> Connect your application -> Copy connection string
-const db = config.get('mongoURI');
+// Look for key value pair `mongoURI`
+const dbUri = keys.mongoURI;
 
 // Async FUnction to Connect to Database
-const connectDB = async() => {
-    try {
-        await mongoose.connect(db, {
-            // Fix Deperication Errors
-            useNewUrlParser: true,
-            useUnifiedTopology: true ,
-            // (node:17188) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
-            useCreateIndex: true
-        });
-        console.log('MOngo DB Connected...');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  await mongoose
+    .connect(dbUri, {
+      // Fix Deperication Errors
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // (node:17188) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+      useCreateIndex: true,
+    })
+    .then((res) => {
+      console.log('MOngo DB Connected...');
+    })
+    .catch((error) => {
+      console.error(error.message);
+      process.exit(1);
+    });
+};
 // Returns a promise with with we can call the above connection code
 // Code is called from server.js
 module.exports = connectDB;
